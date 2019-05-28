cf6c90967fa844a693a945751216bb5c7ab79dfe
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-28 09:00:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -22,16 +22,6 @@ def using_size(array)
 
 end
 
-describe "#using_include" do 
-  it "takes in arguments of an array and an element and returns true if the array includes that element, false if it does not." do 
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    element1 = "wow"
-    element2 = "Tardis"
-    expect(using_include(array, element1)).to eq(true)
-    expect(using_include(array, element2)).to eq(false)
-  end
-end
-
 describe "#using_sort" do 
   it "takes in an argument of an array and returns the sorted array." do 
     array = ["wow", "I", "am", "really", "learning", "arrays!"]
