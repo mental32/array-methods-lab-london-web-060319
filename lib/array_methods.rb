3277d85070f4609dbd6a465ed9b34d1d9c7700ca
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-28 09:01:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,35 +21,3 @@ end
 def using_size(array)
   array.size
 end
-
-describe "#using_reverse" do 
-  it "takes in argument of an array and returns that same array with the contents reversed." do 
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    expect(using_reverse(array).first).to eq("arrays!")
-    expect(using_reverse(array).last).to eq("wow")
-  end
-end
-
-describe "#using_first" do 
-  it "takes in argument of an array and and returns the first element in the array" do 
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    expect(using_first(array)).to eq("wow")
-  end
-end
-
-describe "#using_last" do 
-  it "takes in argument of an array and and returns the last element in the array" do 
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    expect(using_last(array)).to eq("arrays!")
-  end
-end
-
-describe "#using_size" do 
-  it "takes in argument of an array and and returns the size, or length, of the array" do 
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    expect(using_size(array)).to eq(6)
-  end
-end
-
-
-
